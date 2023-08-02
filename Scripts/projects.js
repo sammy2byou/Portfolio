62d26a7fcb23574023f0ae6f7be707b9ca77ba0e
sammy2byou <sammy2bpizza@gmail.com>
2023-08-02 17:10:28 -0400

    Design updated

--- Scripts/projects.js
+++ Scripts/projects.js
@@ -1,3 +1,17 @@
 function notWorking() {
     alert("No public view; not connected to database for back end to work ");
 }
+
+// Get all images elements
+var squareImages = document.querySelectorAll("img");
+
+// Function to set the height of the images to be the same as their width
+function setSquareImagesHeight() {
+    squareImages.forEach(function(image) {
+        var containerWidth = image.parentElement.clientWidth;
+        image.style.height = containerWidth + "px";
+    });
+}
+
+window.addEventListener("load", setSquareImagesHeight);
+window.addEventListener("resize", setSquareImagesHeight);
