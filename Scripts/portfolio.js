2f14572155051e602cb014eab8fbc32810804068
sammy2byou <sammy2bpizza@gmail.com>
2023-08-02 19:13:51 -0400

    Content update
    Code is done

--- Scripts/portfolio.js
+++ Scripts/portfolio.js
@@ -1,1 +1,13 @@
 //JS for all pages
+// Get all images elements
+var squareImages = document.querySelectorAll(".square");
+// Function to set the height of the images to be the same as their width
+function setSquareImagesHeight() {
+    squareImages.forEach(function(image) {
+        var containerWidth = image.parentElement.clientWidth;
+        image.style.height = containerWidth + "px";
+    });
+}
+
+window.addEventListener("load", setSquareImagesHeight);
+window.addEventListener("resize", setSquareImagesHeight);
