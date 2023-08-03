8f3f4fc51415b23a269e3fa6d2b9ae34e7c18ee5
sammy2byou <sammy2bpizza@gmail.com>
2023-08-02 21:30:05 -0400

    Responsive
    Window too small alert

--- Scripts/portfolio.js
+++ Scripts/portfolio.js
@@ -3,6 +3,9 @@
 var squareImages = document.querySelectorAll(".square");
 // Function to set the height of the images to be the same as their width
 function setSquareImagesHeight() {
+    if(window.innerWidth <= 230){
+        alert("Device width is too small\nPage may be inconsistant.");
+    }
     squareImages.forEach(function(image) {
         var containerWidth = image.parentElement.clientWidth;
         image.style.height = containerWidth + "px";
